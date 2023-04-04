c9e06aad83bbaed7540ea8b8b94e9a442bb3daff
Juyoung Lee <juyounglee@Juyoungui-MacBookAir.local>
2023-04-05 00:12:50 +0900

    init - after 6

---
+++ src/main/java/com/example/productorderservice/product/ProductService.java
@@ -0,0 +1,19 @@
+package com.example.productorderservice.product;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Component
+class ProductService {
+    private final ProductPort productPort;
+
+    ProductService(final ProductPort productPort) {
+        this.productPort = productPort;
+    }
+    @Transactional
+    public void addProduct(final AddProductRequest request) {
+        final Product product = new Product(request.name(), request.price(), request.discountPolicy());
+        productPort.save(product);
+    }
+}
