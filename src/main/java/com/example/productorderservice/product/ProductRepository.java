7a6457924a08bb0b79268338ed913e4e91a85ee0
Juyounglee95 <dongry30@gmail.com>
2023-06-09 00:15:39 +0900

    Order service

--- src/main/java/com/example/productorderservice/product/ProductRepository.java
+++ src/main/java/com/example/productorderservice/product/ProductRepository.java
@@ -3,5 +3,5 @@ package com.example.productorderservice.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-interface ProductRepository extends JpaRepository<Product,Long> {
+public interface ProductRepository extends JpaRepository<Product,Long> {
 }
